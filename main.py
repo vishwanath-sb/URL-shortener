eeed75fb10b1e2381ecf8a5869d03a898d383deb
vishwanath sb <vishwanathbhavimani07@gmail.com>
2026-09-16 23:21:40 +0530

    get API is implemented

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
-class URLRequest(BaseModel):
-    url:str
 
+class URLRequest(BaseModel):
+    url: str
+
+
+# In-memory storage
 url_mapping = {}
+
+# Counter for generating unique IDs
 counter = 1
+
 
 # Base62 characters
 CHARACTERS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
@@ -29,7 +36,8 @@ def base62_encode(number):
 
 @app.get("/")
 def home():
-    return {"message" : "URL shortener"}
+    return {"message": "URL Shortener"}
+
 
 @app.post("/shorten")
 def shorten_url(request: URLRequest):
@@ -41,10 +49,27 @@ def shorten_url(request: URLRequest):
     # Store mapping
     url_mapping[short_id] = request.url
 
-    # Increment counter for next URL
+    # Increment counter
     counter += 1
 
     return {
         "short_id": short_id,
         "url": request.url
     }
+
+
+@app.get("/{short_id}")
+def redirect_url(short_id: str):
+
+    if short_id not in url_mapping:
+        raise HTTPException(
+            status_code=404,
+            detail="short url not found"
+        )
+
+    url = url_mapping[short_id]
+
+    return RedirectResponse(
+        url=url,
+        status_code=307
+    )
